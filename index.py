dc3af00f0bedf0fe615d0377ce9d4a9096ab0764
Saiteja-Reddy <saiteja0317@gmail.com>
2018-08-23 13:41:44 +0530

    Cleaned Code

--- index.py
+++ index.py
@@ -6,9 +6,7 @@ import sys
 program_name = sys.argv[0]
 arguments = sys.argv[1:]
 count = len(arguments)
-# print(arguments)
 
-# from nltk.corpus import stopwords
 s_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 s_words.append("name")
 s_words.append("br")
@@ -49,7 +47,6 @@ def process_body(fin):
     fin = re.sub("<blockquote.*?>(.*?)</blockquote>", r"\1 ", fin)
     fin = fin.rstrip()
     fin = re.sub("{{verify.*?}}", " ", fin, re.DOTALL)
-    # fin = re.sub("{{citation.*?}}", " ", fin, )
     fin = re.sub("{{failed.*?}}", " ", fin, re.DOTALL)
     fin = re.sub("{{page.*?}}", " ", fin, re.DOTALL)
     fin = re.sub("{{lang.*?fa.*?}}", " ", fin, re.DOTALL)
@@ -69,9 +66,6 @@ def process(page_text, page_title, page_id):
     page_text = remove_non_ascii(page_text)
     page_title = page_title.encode("utf-8", "ignore")    
     page_title = remove_non_ascii(page_title)    
-    # f = open('text_orig.txt', 'w')
-    # f.write(page_text)
-    # f.close()   
 
     page_text = page_text.lower()
 
@@ -88,8 +82,6 @@ def process(page_text, page_title, page_id):
     references = process_body(references)
 
     page_text = links_match.sub("", page_text)
-
-    # infoboxes = infobox_match.findall(page_text)
 
     infoboxes = []
     positions = []
@@ -119,7 +111,6 @@ def process(page_text, page_title, page_id):
             infoboxes += sp.split("=")[1] + " "
         except:
             infoboxes += sp + " " 
-    # unprocessed = infoboxes
     infoboxes = process_body(infoboxes)
 
     category = page_cat_match.findall(page_text)
@@ -139,8 +130,6 @@ def process(page_text, page_title, page_id):
     ext_links = links_match.sub("", ext_links)
     ext_links = process_body(ext_links)
 
-    # print(ext_links)
-
     further_read = further_read_match.findall(page_text, re.DOTALL)
     page_text = re.sub(further_read_match, ' ', page_text)
     further_read = " ".join(further_read)
@@ -155,7 +144,6 @@ def process(page_text, page_title, page_id):
     see_also = process_body(see_also)
 
 
-    # cites = [ re.match(".*?title\s?=(.*?)\|.*", temp, re.DOTALL).groups()[0] for temp in cites if re.match(".*?title\s?=(.*?)\|.*", temp, re.DOTALL) ]
     now_cites = cites
     cites = ""
     for temp in now_cites:
@@ -170,44 +158,10 @@ def process(page_text, page_title, page_id):
     cites = links_match.sub("", cites)                    
     cites = process_body(cites)
 
-    # f = open('text_other.txt', 'w')
-    # f.write("cites\n")
-    # f.write("\n" + cites)
-    # f.write("\n\nunproecssesinfoboxes\n")
-    # f.write(unprocessed)
-    # f.write("\n\ninfoboxes\n")
-    # f.write(infoboxes)
-    # f.write("\n\ncategory\n")
-    # f.write("\n" + category)
-    # f.write("\n\nnotes_and_refs\n")
-    # if(notes_and_refs):
-    #     f.write(notes_and_refs)
-    # f.write("\n\next_links\n")
-    # if(ext_links):
-    #     f.write(ext_links)
-    # f.write("\n\nfurther_read\n") 
-    # if(further_read): 
-    #     f.write(further_read)
-    # f.write("\n\nsee_also\n")  
-    # if(see_also):
-    #     f.write(see_also)
-    # f.write("\n\nreferences\n")  
-    # f.write("\n" + references)
-    # f.write("\n\n")               
-    # f.close()      
-
     references = cites + " " + references + " " + see_also + " " + further_read + " " + notes_and_refs                                                              
 
 
-    # f = open('text_bp.txt', 'w')
-    # f.write(page_text)
-    # f.close()
-
     page_text = process_body(page_text)
-    # print("Heere")
-    # f = open('text.txt', 'w')
-    # f.write(page_text)
-    # f.close() 
 
     page_title = camel_case_split(page_title)
     page_title = page_title.translate(translator)
@@ -223,8 +177,6 @@ def process(page_text, page_title, page_id):
     tokens = [token for token in tokens if token not in s_words]
     stemmed_tokens = stemmer.stemWords(tokens)
     title_counts = Counter(stemmed_tokens)
-    # print(title_counts)
-
 
     tokens = category.split()
     tokens = [token for token in tokens if token not in s_words]
@@ -240,7 +192,6 @@ def process(page_text, page_title, page_id):
     tokens = [token for token in tokens if token not in s_words]
     stemmed_tokens = stemmer.stemWords(tokens)
     link_counts = Counter(stemmed_tokens)
-    # print(link_counts)
 
     tokens = references.split()
     tokens = [token for token in tokens if token not in s_words]
@@ -253,9 +204,6 @@ def process(page_text, page_title, page_id):
     all_tokens = all_tokens.union(info_counts.keys())
     all_tokens = all_tokens.union(link_counts.keys())
     all_tokens = all_tokens.union(ref_counts.keys())
-    # print(len(body_counts.keys()))
-    # print(len(all_tokens))
-
 
     for k in all_tokens:
         counts = str(page_id)
@@ -306,14 +254,6 @@ for event, elem in context:
     elem.clear()
     # if(page_id == "6635"):
         # break
-
-# import json
-# with open("json_inverted", 'w') as f:
-#     json.dump(inverted_index, f)
-
-# import pickle
-# with open('index.pickle', 'wb') as handle:
-#     pickle.dump(inverted_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 print("writing to " + arguments[1])
 f = open(arguments[1], 'w')
