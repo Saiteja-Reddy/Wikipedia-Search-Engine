167b220bef4ed4b5c46a5dbe0f3bd76189e98f54
Saiteja-Reddy <saiteja0317@gmail.com>
2018-08-23 12:36:37 +0530

    Submission Format made

--- index.py
+++ index.py
@@ -2,6 +2,12 @@ from lxml import etree
 import re
 import string
 
+import sys
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+count = len(arguments)
+# print(arguments)
+
 # from nltk.corpus import stopwords
 s_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 s_words.append("name")
@@ -271,7 +277,8 @@ def process(page_text, page_title, page_id):
             inverted_index[k] = counts + ","
 
 
-infile = "wiki-search-small.xml"
+infile = arguments[0]
+print("using file " + infile)
  
 count = 0
 
@@ -293,7 +300,7 @@ for event, elem in context:
             for sub in child:
                 if sub.tag == "{http://www.mediawiki.org/xml/export-0.8/}text":
                     page_text = sub.text
-    print(str(count) + " " + " " + page_id + " " +  page_title + "\n")                    
+    # print(str(count) + " " + " " + page_id + " " +  page_title + "\n")                    
     if(page_text != None):
         process(page_text , page_title, page_id)
     elem.clear()
@@ -308,7 +315,8 @@ for event, elem in context:
 # with open('index.pickle', 'wb') as handle:
 #     pickle.dump(inverted_index, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-f = open('invert_index.txt', 'w')
+print("writing to " + arguments[1])
+f = open(arguments[1], 'w')
 for key in sorted(inverted_index):
     f.write(key + " " + inverted_index[key] + "\n")
 f.close()
---
+++ index.sh
@@ -0,0 +1,1 @@
+python2 index.py $1 $2
