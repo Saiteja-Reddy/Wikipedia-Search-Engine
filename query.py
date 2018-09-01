6141268b3e681e4e0e9cfb0de90b1813369da659
Saiteja-Reddy <saiteja0317@gmail.com>
2018-09-01 16:09:55 +0530

    added ranking for fields

--- query.py
+++ query.py
@@ -1,6 +1,8 @@
 word = "alabama"
 
 import os
+import heapq
+from math import log10
 
 def get_word_posting(index, word):
 	f = open(index, 'r')
@@ -33,23 +35,63 @@ if(token == -1):
 	print("Not Found")
 else:
 	print(token + " " + occur + " " + posting)
+	heap = []
+	field = 'b'
+	top_count = 10
+	# category = 'b'
 	posting = posting.split("\n")[0]
 	posting = posting.split(",")[:-1]
+	ref_count, links_count, info_count, category_count, title_count, body_count = [0 for _ in range(6)]
 	for post in posting:
+		# print post
 		ref, links, info, category, title, body = [0 for _ in range(6)]
 		if(post.find('r') is not -1):
 			post, ref = post.split('r')
+			ref_count = ref_count + 1
 		if(post.find('l') is not -1):
 			post, links = post.split('l')
+			links_count = links_count + 1
 		if(post.find('i') is not -1):
 			post, info = post.split('i')
+			info_count = info_count + 1
 		if(post.find('c') is not -1):			
 			post, category = post.split('c')
+			category_count = category_count + 1
 		if(post.find('t') is not -1):			
 			post, title = post.split('t')
+			title_count = title_count + 1
 		if(post.find('b') is not -1):	
 			post, body = post.split('b')
-		print(post + " b - " + str(body) + " t - " + str(title) + " c - " + str(category) + " i - " + str(info) + " l - " + str(links) + " r - " + str(ref))
-	# print(posting)
+			body_count = body_count + 1
+
+		if field == 'b':
+			tf_idf_val = log10(1 + int(body)) * log10(5311/ body_count)
+		elif field == 't':
+			tf_idf_val = log10(1 + int(title)) * log10(5311/ title_count)
+		elif field == 'c':
+			tf_idf_val = log10(1 + int(category)) * log10(5311/ category_count)
+		elif field == 'i':
+			tf_idf_val = log10(1 + int(info)) * log10(5311/ info_count)
+		elif field == 'l':
+			tf_idf_val = log10(1 + int(links)) * log10(5311/ links_count)	
+		elif field == 'r':
+			tf_idf_val = log10(1 + int(ref)) * log10(5311/ ref_count)														
+
+		if(len(heap) < top_count):
+			heapq.heappush(heap, [tf_idf_val, int(post)])
+			# print(heap)
+		else:
+			if(heap[0][0] < tf_idf_val):
+				heapq.heappop(heap)
+				heapq.heappush(heap, [tf_idf_val, int(post)])
+				# print(len(heap))
+		# print(post + " b - " + str(body) + " t - " + str(title) + " c - " + str(category) + " i - " + str(info) + " l - " + str(links) + " r - " + str(ref))
+	
+	req = top_count
+	# print(heap)
+	while(len(heap) > 0 and req > 0):
+		req = req - 1
+		print(heapq.heappop(heap))
+	# print(heap)
 
 
