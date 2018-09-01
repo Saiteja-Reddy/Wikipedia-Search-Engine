fbe5b625a46972009ce01c3dfdc8d0129808c63c
Saiteja-Reddy <saiteja0317@gmail.com>
2018-09-01 15:12:43 +0530

    split posting

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-word = "navconstel1231"
+word = "alabama"
 
 import os
 
@@ -9,7 +9,7 @@ def get_word_posting(index, word):
 	r = index_size
 	while l<r:
 		m = (l+r)/2
-		print(int(m))
+		# print(int(m))
 		f.seek(int(m))
 		line = f.readline()
 		if(line == ""):
@@ -33,3 +33,23 @@ if(token == -1):
 	print("Not Found")
 else:
 	print(token + " " + occur + " " + posting)
+	posting = posting.split("\n")[0]
+	posting = posting.split(",")[:-1]
+	for post in posting:
+		ref, links, info, category, title, body = [0 for _ in range(6)]
+		if(post.find('r') is not -1):
+			post, ref = post.split('r')
+		if(post.find('l') is not -1):
+			post, links = post.split('l')
+		if(post.find('i') is not -1):
+			post, info = post.split('i')
+		if(post.find('c') is not -1):			
+			post, category = post.split('c')
+		if(post.find('t') is not -1):			
+			post, title = post.split('t')
+		if(post.find('b') is not -1):	
+			post, body = post.split('b')
+		print(post + " b - " + str(body) + " t - " + str(title) + " c - " + str(category) + " i - " + str(info) + " l - " + str(links) + " r - " + str(ref))
+	# print(posting)
+
+
