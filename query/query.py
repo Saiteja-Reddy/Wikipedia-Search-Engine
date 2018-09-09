a3c1ca8a803b9f82142eb7277383564f9c6a2989
Saiteja-Reddy <saiteja0317@gmail.com>
2018-09-09 23:57:28 +0530

    final query

--- query/query.py
+++ query/query.py
@@ -4,6 +4,7 @@ import heapq
 from math import log10
 import time
 import string
+import operator
 
 s_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 s_words.append("name")
@@ -25,7 +26,7 @@ def get_word_posting(index, word):
 		m = (l+r)/2
 		m = int(m)
 		# print(int(m))
-		print(m)
+		# print(m)
 		f.seek(m)
 		line = f.readline()
 		if(line == ""):
@@ -65,7 +66,7 @@ def get_title(t_id, filename = "out_titles.txt"):
 		line = f.readline()
 		output = line.split(",")
 		if(int(output[0]) == t_id):
-			print(output)
+			# print(output)
 			f.close()
 			return output
 			break
@@ -78,19 +79,19 @@ def get_title(t_id, filename = "out_titles.txt"):
 
 def process_word(word, field):
 	if field == "t":
-		file_name = "indexes/600_tf_index_title.txt"
+		file_name = "indexes/tf_all_index/tf_index_title.txt"
 	elif field == "b":
-		file_name = "indexes/600_tf_index_body.txt"
+		file_name = "indexes/tf_all_index/tf_index_body.txt"
 	elif field == "c":
-		file_name = "indexes/600_tf_index_category.txt"
+		file_name = "indexes/tf_all_index/tf_index_category.txt"
 	elif field == "i":
-		file_name = "indexes/600_tf_index_infobox.txt"
+		file_name = "indexes/tf_all_index/tf_index_infobox.txt"
 	elif field == "l":
-		file_name = "indexes/600_tf_index_links.txt"
+		file_name = "indexes/tf_all_index/tf_index_links.txt"
 	elif field == "r":
-		file_name = "indexes/600_tf_index_references.txt"
+		file_name = "indexes/tf_all_index/tf_index_references.txt"
 	else:
-		file_name = "indexes/600_tf_index_all.txt"												
+		file_name = "indexes/tf_all_index/tf_index_all.txt"												
 
 	token, idf,  posting =  get_word_posting(file_name, word)
 	# print(posting)
@@ -110,5 +111,78 @@ def process_word(word, field):
 	return tuples
 
 
-process_word("telangana", "a")
+# process_word("telangana", "a")
 # print("done")
+
+def process_query(queries):
+	query_arrays = []
+	for query in queries:
+		sym = query.split(":")
+		if(len(sym) == 1):
+			# sym[0] = sym[0].translate(translator)
+			sym[0] = stemmer.stemWord(sym[0].lower())
+			pl = process_word(sym[0], 'a')
+			# print(pl)
+			if(len(pl) == 0):
+				continue
+			query_arrays.append(pl)
+			# print(('a', sym[0]))
+		else:
+			# sym[0] = sym[0].translate(translator)
+			sym[1] = stemmer.stemWord(sym[1].lower())
+			pl = process_word(sym[1], sym[0])
+			if(len(pl) == 0):
+				continue			
+			query_arrays.append(pl)
+			# print((sym[0], sym[1]))
+
+	# print(query_arrays)
+	# print(len(query_arrays))
+	if(len(query_arrays) == 0):
+		sorted_ranks = {}
+	elif len(query_arrays) == 1:
+		docs = set([i[0] for i in query_arrays[0]])
+		docs = docs.intersection([i[0] for i in query_arrays[0]])
+		ranks = {}
+		for doc in docs:
+			ranks[doc] = 0.0
+		for query_array in query_arrays:
+			for i in query_array:
+				if i[0] in docs:
+					ranks[i[0]] = ranks[i[0]] + float(i[1])		
+		sorted_ranks = sorted(ranks.items(), key=operator.itemgetter(1), reverse=True)
+
+	else:
+		docs = set([i[0] for i in query_arrays[0]])
+		for query_array in query_arrays:
+			# print(len(query_array))
+			docs = docs.intersection([i[0] for i in query_array])
+		# print(len(docs))
+		ranks = {}
+		for doc in docs:
+			ranks[doc] = 0.0
+		# print(ranks)
+		for query_array in query_arrays:
+			for i in query_array:
+				if i[0] in docs:
+					ranks[i[0]] = ranks[i[0]] + float(i[1])
+
+		# print(ranks)
+		sorted_ranks = sorted(ranks.items(), key=operator.itemgetter(1), reverse=True)
+		# print sorted_ranks
+
+	if(len(sorted_ranks) >= 10):
+		# print(sorted_ranks[:10])
+		for now in sorted_ranks[:10]:
+			print(get_title(int(now[0])))
+	else:
+		print("No Results!")
+	
+
+
+while True:
+	queries = raw_input("Query-> ")
+	queries = queries.split()
+	start_time = time.time()
+	process_query(queries)
+	print("Process time : " + str(time.time()-start_time))
